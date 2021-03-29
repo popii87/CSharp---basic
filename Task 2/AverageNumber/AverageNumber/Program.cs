d1cca632087b480bbec1699ab1c4b5f170252ee6
Robert Dimov <robertdimov87@gmail.com>
2021-03-29 21:52:48 +0200

    Task 1,2,3 added

---
+++ Task 2/AverageNumber/AverageNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AverageNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number");
+            int num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number");
+            int num4 = int.Parse(Console.ReadLine());
+            int average = (num1 + num2 + num3 + num4) / 4;
+            Console.WriteLine($"The average of {num1}, {num2}, {num3}, and {num4} is: {average} ");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Task 3/SwapNumbers/SwapNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SwapNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, temp;
+            Console.WriteLine("Input the First Number : ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the Second Number : ");
+            number2 = int.Parse(Console.ReadLine());
+            temp = number1;
+            number1 = number2;
+            number2 = temp;
+            Console.WriteLine("After Swapping : ");
+            Console.WriteLine("First Number : " + number1);
+            Console.WriteLine("Second Number : " + number2);
+            Console.ReadLine();
+        }
+    }
+}

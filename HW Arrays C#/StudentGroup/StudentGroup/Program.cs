6b23212347a804a4ca609dd39e09e98d3f7a250c
Robert Dimov <robertdimov87@gmail.com>
2021-03-31 20:20:04 +0200

    Home Work added

---
+++ HW Arrays C#/StudentGroup/StudentGroup/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+            string[] studentsG2 = { "Anja", "Sara", "Nevenka", "Stefanija", "Marko" };
+            Console.WriteLine("Enter student group: (there are 1 and 2)");
+            string getInput = Console.ReadLine();
+            int numOfGroup;
+            bool checkInput = int.TryParse(getInput, out numOfGroup);
+            switch (numOfGroup)
+            {
+                case 1:
+                    Console.WriteLine("The students in G1 are:");
+                    foreach (string student in studentsG1)
+                    {
+                        
+                        Console.WriteLine(student);
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("The students in G2 are:");
+                    foreach (string student in studentsG2)
+                    {
+
+                        Console.WriteLine(student);
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ HW Arrays C#/SumOfEven/SumOfEven/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[6];
+            Console.WriteLine("Enter 6 numbers");
+            string getInput = Console.ReadLine();
+            bool checkInput = int.TryParse(getInput, out numbers[0]);
+            string getInput2 = Console.ReadLine();
+            bool checkInput2 = int.TryParse(getInput2, out numbers[1]);
+            string getInput3 = Console.ReadLine();
+            bool checkInput3 = int.TryParse(getInput3, out numbers[2]);
+            string getInput4 = Console.ReadLine();
+            bool checkInput4 = int.TryParse(getInput4, out numbers[3]);
+            string getInput5 = Console.ReadLine();
+            bool checkInput5 = int.TryParse(getInput5, out numbers[4]);
+            string getInput6 = Console.ReadLine();
+            bool checkInput6 = int.TryParse(getInput6, out numbers[5]);
+            int result = 0;
+            foreach (int num in numbers)
+            {
+                result += num;
+
+            }
+            Console.WriteLine($"The result is: {result}");
+
+        }
+    }
+}
